2c2d74353612c620632131f4e28fc3f0d83da6e5
urosrelic <urosrelicc@gmail.com>
2024-08-24 17:07:33 +0200

    add user micro service

---
+++ user/src/main/java/com/urosrelic/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.urosrelic.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
