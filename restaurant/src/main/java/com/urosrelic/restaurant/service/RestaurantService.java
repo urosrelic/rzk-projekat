6d9ad89701482e36b28a8aff7dcefda8b558ddc3
urosrelic <urosrelicc@gmail.com>
2024-08-27 16:37:44 +0200

    add addRestaurant and existsById methods

---
+++ restaurant/src/main/java/com/urosrelic/restaurant/service/RestaurantService.java
@@ -0,0 +1,23 @@
+package com.urosrelic.restaurant.service;
+
+import com.urosrelic.restaurant.dto.RestaurantCreationRequest;
+import com.urosrelic.restaurant.model.Restaurant;
+import com.urosrelic.restaurant.repository.RestaurantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RestaurantService {
+    private final RestaurantRepository restaurantRepository;
+
+    public Restaurant addRestaurant(RestaurantCreationRequest request) {
+        Restaurant restaurant = new Restaurant();
+        restaurant.setName(request.getName());
+        return restaurantRepository.save(restaurant);
+    }
+
+    public boolean existsById(String id) {
+        return restaurantRepository.existsById(id);
+    }
+}
