413820c21c10e2982eba541a46618ddf8544919c
urosrelic <urosrelicc@gmail.com>
2024-08-27 17:37:24 +0200

    fix paths in category feign client

--- food/src/main/java/com/urosrelic/food/client/CategoryClient.java
+++ food/src/main/java/com/urosrelic/food/client/CategoryClient.java
@@ -5,14 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "category-service")
 public interface CategoryClient {
-    @PostMapping("/getByIds")
+    @GetMapping("/api/v1/category/getByIds")
     ResponseEntity<?> getCategoriesByIds(@RequestBody GetCategoriesByIdsRequest request);
 
-    @GetMapping("/exists/{id}")
+    @GetMapping("/api/v1/category/exists/{id}")
     boolean existsById(@PathVariable String id);
 }
