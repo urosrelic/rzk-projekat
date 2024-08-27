fbb338f65991e07b9f68da8ae50d92b0cc663809
urosrelic <urosrelicc@gmail.com>
2024-08-27 16:41:30 +0200

    add responses to methods

--- restaurant/src/main/java/com/urosrelic/restaurant/controller/RestaurantController.java
+++ restaurant/src/main/java/com/urosrelic/restaurant/controller/RestaurantController.java
@@ -1,8 +1,13 @@
 package com.urosrelic.restaurant.controller;
 
 import com.urosrelic.restaurant.dto.RestaurantCreationRequest;
+import com.urosrelic.restaurant.handler.ResponseHandler;
+import com.urosrelic.restaurant.handler.ResponseType;
+import com.urosrelic.restaurant.model.Restaurant;
 import com.urosrelic.restaurant.service.RestaurantService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,12 +18,24 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @PostMapping
-    public void addRestaurant(@RequestBody RestaurantCreationRequest request) {
-        restaurantService.addRestaurant(request);
+    public ResponseEntity<?> addRestaurant(@RequestBody RestaurantCreationRequest request) {
+        Restaurant restaurant = restaurantService.addRestaurant(request);
+
+        if (restaurant == null) {
+            return ResponseHandler.generateResponse(ResponseType.ERROR, "Error adding restaurant", HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseHandler.generateResponseWithBody(ResponseType.SUCCESS, "Restaurant created", HttpStatus.CREATED, restaurant);
     }
 
     @GetMapping("/{id}")
-    public boolean existsById(@PathVariable String id) {
-        return restaurantService.existsById(id);
+    public ResponseEntity<?> existsById(@PathVariable String id) {
+        boolean exists = restaurantService.existsById(id);
+
+        if (exists) {
+            return ResponseHandler.generateResponseWithBody(ResponseType.SUCCESS, "Restaurant exists", HttpStatus.OK, true);
+        } else {
+            return ResponseHandler.generateResponseWithBody(ResponseType.SUCCESS, "Restaurant does not exist", HttpStatus.OK, false);
+        }
     }
 }
