a1e95111112d06dea159a76221930e8092308fd2
urosrelic <urosrelicc@gmail.com>
2024-08-28 02:42:07 +0200

    add cart model

---
+++ cart/src/main/java/com/urosrelic/cart/model/Cart.java
@@ -0,0 +1,31 @@
+package com.urosrelic.cart.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    @Id
+    private String id;
+    private String user;
+    private List<CartItem> cartItems;
+    private double cartTotal;
+    private boolean confirmed;
+
+    public Cart(String user, List<CartItem> cartItems, double cartTotal, boolean confirmed) {
+        this.user = user;
+        this.cartItems = cartItems;
+        this.cartTotal = cartTotal;
+        this.confirmed = confirmed;
+    }
+}
