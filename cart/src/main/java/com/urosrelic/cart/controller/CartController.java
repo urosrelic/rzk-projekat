d00f1f0deda9b5cbca6c7923522d6192f07b2bf4
urosrelic <urosrelicc@gmail.com>
2024-08-28 03:04:46 +0200

    throw exceptions instead of ResponseEntity in service

--- cart/src/main/java/com/urosrelic/cart/controller/CartController.java
+++ cart/src/main/java/com/urosrelic/cart/controller/CartController.java
@@ -1,8 +1,12 @@
 package com.urosrelic.cart.controller;
 
 import com.urosrelic.cart.dto.AddToCartRequest;
+import com.urosrelic.cart.handler.ResponseHandler;
+import com.urosrelic.cart.handler.ResponseType;
+import com.urosrelic.cart.model.Cart;
 import com.urosrelic.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +18,7 @@ public class CartController {
 
     @PostMapping
     public ResponseEntity<Object> addToCart(@RequestBody AddToCartRequest request, @RequestHeader("Authorization") String token) {
-        String foodId = request.getFoodId();
-        int quantity = request.getQuantity();
-        return cartService.addToCart(foodId, quantity, token);
+        Cart cart = cartService.addToCart(request.getFoodId(), request.getQuantity(), token);
+        return ResponseHandler.generateResponseWithBody(ResponseType.SUCCESS, "Item added to cart", HttpStatus.OK, cart);
     }
 }
--- cart/src/main/java/com/urosrelic/cart/service/CartService.java
+++ cart/src/main/java/com/urosrelic/cart/service/CartService.java
@@ -4,15 +4,14 @@ import com.urosrelic.cart.beans.Food;
 import com.urosrelic.cart.beans.User;
 import com.urosrelic.cart.client.AuthClient;
 import com.urosrelic.cart.client.FoodClient;
-import com.urosrelic.cart.handler.ResponseHandler;
-import com.urosrelic.cart.handler.ResponseType;
-import com.urosrelic.cart.model.Cart;
 import com.urosrelic.cart.dto.CartItem;
+import com.urosrelic.cart.exception.CartItemAlreadyExistsException;
+import com.urosrelic.cart.exception.FoodNotFoundException;
+import com.urosrelic.cart.exception.UserNotFoundException;
+import com.urosrelic.cart.model.Cart;
 import com.urosrelic.cart.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,22 +25,25 @@ public class CartService {
     private final AuthClient authClient;
     private final FoodClient foodClient;
 
-    public ResponseEntity<Object> addToCart(String foodId, int quantity, String token) {
-        User user = authClient.getUser(token);
+    public Cart addToCart(String foodId, int quantity, String token) {
+        User user;
+        try {
+            user = authClient.getUser(token);
+        } catch (Exception ex) {
+            throw new UserNotFoundException("User not found");
+        }
 
         Optional<Food> foodOptional = foodClient.getFoodById(foodId);
         if (foodOptional.isEmpty()) {
-            return ResponseHandler.generateResponse(ResponseType.ERROR, "Food not found", HttpStatus.NOT_FOUND);
+            throw new FoodNotFoundException("Food not found");
         }
 
         Food food = foodOptional.get();
         Cart cart = cartRepository.findByUserAndConfirmedFalse(user.getId());
 
-        double totalPrice = 0;
-
         if (cart == null) {
             CartItem createdCartItem = new CartItem(foodId, quantity, food.getPrice());
-            totalPrice = createdCartItem.getPrice() * createdCartItem.getQuantity();
+            double totalPrice = createdCartItem.getPrice() * createdCartItem.getQuantity();
 
             cart = new Cart(user.getId(), List.of(createdCartItem), totalPrice, false);
             cartRepository.save(cart);
@@ -51,30 +53,23 @@ public class CartService {
                     .findFirst();
 
             if (existingCartItemOptional.isPresent()) {
-                return ResponseHandler.generateResponse(ResponseType.ERROR, "Item already in cart", HttpStatus.BAD_REQUEST);
+                throw new CartItemAlreadyExistsException("Item already in cart");
             } else {
                 CartItem createdCartItem = new CartItem(foodId, quantity, food.getPrice());
                 cart.getCartItems().add(createdCartItem);
                 cartRepository.save(cart);
 
-                totalPrice += food.getPrice() * quantity;
-            }
+                double totalPrice = 0;
+                for (CartItem item : cart.getCartItems()) {
+                    totalPrice += item.getPrice() * item.getQuantity();
+                }
 
-            totalPrice = 0;
-            for (CartItem item : cart.getCartItems()) {
-                totalPrice += item.getPrice() * item.getQuantity();
+                cart.setCartTotal(totalPrice);
+                cartRepository.save(cart);
             }
-
-            cart.setCartTotal(totalPrice);
-            cartRepository.save(cart);
         }
 
-        Optional<Cart> populatedCart = cartRepository.findById(cart.getId());
-
-        if (populatedCart.isEmpty()) {
-            return ResponseHandler.generateResponse(ResponseType.ERROR, "Error populating cart", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        return new ResponseEntity<>(populatedCart, HttpStatus.OK);
+        return cartRepository.findById(cart.getId())
+                .orElseThrow(() -> new RuntimeException("Error adding item to cart"));
     }
 }
