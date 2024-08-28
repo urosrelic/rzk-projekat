c5c8d6252a3ce2f36a1d0dbbebc6de26fec21324
urosrelic <urosrelicc@gmail.com>
2024-08-28 02:41:50 +0200

    add dtos

---
+++ cart/src/main/java/com/urosrelic/cart/beans/Food.java
@@ -0,0 +1,11 @@
+package com.urosrelic.cart.beans;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Food {
+    private String id;
+    private String name;
+    private double price;
+}
---
+++ cart/src/main/java/com/urosrelic/cart/beans/User.java
@@ -0,0 +1,13 @@
+package com.urosrelic.cart.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String id;
+    private String name;
+    private String lastName;
+    private String email;
+}
